193a919be08f4321dec8eee76886a693cce9bc76
lfcanals <luisf.canals@gmail.co>
2020-04-27 18:17:37 +0200

    Addded server features

--- gcloud/server.js
+++ gcloud/server.js
@@ -6,10 +6,19 @@ const bodyParser = require('body-parser')
 // Milliseconds to keep in memory ids in the same geoposition
 const LAPSE = 10000; // 10 seconds. Why? Because there are 10 types of
                      // humans: those who know binary and those who don't
+//
+const INFECTION_MEMORY = 5 * 3600 * 24;  // 5 days of memory of an infection
+
+
+// Short term (no older than LAPSE) memory of neighborhood
+//    hashedGPS -> [{hashId, time},...]
 const geoPos = {};
 
+// Short term (no older than INFECTION TIME) memory for notified infections
+//    [{time, infected hashId}...]
+const infections = [];
 
-app.use(bodyParser.json({type: '*/json'}));
+
 
 // For debuugging
 app.all('/echo', (req, res) => {
@@ -17,9 +26,22 @@ app.all('/echo', (req, res) => {
 });
 
 // For justice and freedom
-app.get('/about', (req, res) => {
-  res.send('Freedom in Spain now! Stop the coup d\'etat.');
+app.get('/help', (req, res) => {
+  res.send('Freedom in Spain now! "Confinement" Confinement is not an ilegal massive house arrest and policial cup d\'etat');
 });
+
+// For telling you the instructions
+app.get('/info', (req, res) => {
+  let msg = '<html><body>Download the apk ';
+  msg += '<a href="static/app-debug.apk">here</a>';
+  msg += '<br/>Notify an infection for a hashId: ' 
+  msg += '<a href="static/notify-infection.html">here</a>';
+  msg += '</body></html>';
+  res.send(msg);
+});
+
+
+
 
 // For asking for neighbors
 // response: a json with : { "nearIds" : [ array of { "id", "time" } ] }
@@ -51,6 +73,38 @@ app.post('/geopos', (req, res) => {
 });
 
 
+// For asking for an infection 
+app.post('/infection', (req, res) => {
+  const now = Date.now();
+  const minTime = now - INFECTION_MEMORY;
+  const hashInfectedIdsArray = [];
+  for(var i = infections.length-1; i>=0; i--) {
+    if(infections[i][0] < minTime) {
+      infections.splice(i, 1);
+    } else {
+      hashInfectedIdsArray.append(infections[i][1]);
+    }
+  }
+
+  res.json({ infectedIds : hashInfectedIdsArray });
+});
+
+
+// For notifying of an infection 
+app.get('/notify-infection', (req, res) => {
+  var hashId = req.param("hashId", null);
+  if(hashId==null) {
+    res.send("Receive a null in parameter hashId");
+  } else {
+    infections.push([Date.now(), hashId]);
+    res.send("Received and stored infection for hashId " + hashId);
+  }
+});
+
+
+
+// The server, ready to run
+app.use(bodyParser.json({type: '*/json'}));
 const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
